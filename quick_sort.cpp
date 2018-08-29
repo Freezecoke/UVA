095d91dc7ad67d8d6ff846ab2e576113b368c546
Jason <gtopopjason@gmail.com>
2018-08-29 11:42:56 +0800

    version 0.2

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -3,5 +3,23 @@ using namespace std;
 
 int main(){
 
+    int A[10]={5,4,9,6,7,2,4,3,5,1};
+    quick_sort(A,0,9);
+    for(int i=0;i<10;i++)
+       cout<<A[i];
+    cout <<endl;
+
+}
+
+void quick_sort(int a[], int left, int right){
+
+  int i = left;
+  int j = right;
+  int tmp;
+  int pivot = a[ (left+right)/2 ];
+
+  test
+  
+  
 
 }
