01458bebb1dc0426203198793dbe2b256b42e038
Jason <gtopopjason@gmail.com>
2018-08-31 12:46:36 +0800

    version 0.3

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
 using namespace std;
 
+void quick_sort(int a[], int left, int right);
+int partition(int a[], int left, int right);
+
 int main(){
 
-    int A[10]={5,4,9,6,7,2,4,3,5,1};
-    quick_sort(A,0,9);
-    for(int i=0;i<10;i++)
+    //int A[10]={5,4,9,6,7,2,4,3,5,1};
+    int A[9]={6,5,1,3,8,4,7,9,2};
+    quick_sort(A,0,8);
+    for(int i=0;i<9;i++)
        cout<<A[i];
     cout <<endl;
 
@@ -13,13 +17,38 @@ int main(){
 
 void quick_sort(int a[], int left, int right){
 
-  int i = left;
-  int j = right;
+  int pivot;
+
+  if(left<right){
+    pivot = partition(a, left, right);
+    quick_sort(a, left, pivot-1);
+    quick_sort(a, pivot+1, right);
+  }
+
+}
+
+int partition(int a[], int left, int right){
+
+  int i = left+1;
+  int j;
   int tmp;
-  int pivot = a[ (left+right)/2 ];
 
-  test
-  
-  
-
+  for(j=left+1; j<=right; j++){
+    if(a[left]>=a[j]){
+       //cout << i << endl;
+       //cout << j << endl;
+       //cout << a[j] << endl;
+       tmp = a[i];
+       a[i] = a[j];
+       a[j] = tmp;
+       i++;
+    }
+  }
+  tmp = a[i-1];
+  a[i-1] = a[left];
+  a[left] = tmp;
+  //cout << i << endl;
+  //cout << "left= " << left << endl;
+  //cout << "right= " << right << endl;
+  return i-1;
 }
