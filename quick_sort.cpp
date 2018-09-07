68caa95920bee71a1fc72f62903fbb50a52233a8
Jason <gtopopjason@gmail.com>
2018-09-07 15:28:04 +0800

    version 1.0

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -6,7 +6,6 @@ int partition(int a[], int left, int right);
 
 int main(){
 
-    //int A[10]={5,4,9,6,7,2,4,3,5,1};
     int A[9]={6,5,1,3,8,4,7,9,2};
     quick_sort(A,0,8);
     for(int i=0;i<9;i++)
@@ -29,26 +28,21 @@ void quick_sort(int a[], int left, int right){
 
 int partition(int a[], int left, int right){
 
-  int i = left+1;
+  int i = left+1;//wall
   int j;
   int tmp;
 
   for(j=left+1; j<=right; j++){
     if(a[left]>=a[j]){
-       //cout << i << endl;
-       //cout << j << endl;
-       //cout << a[j] << endl;
        tmp = a[i];
        a[i] = a[j];
        a[j] = tmp;
        i++;
     }
   }
+  //change pivot to the middle, which means wall-1
   tmp = a[i-1];
   a[i-1] = a[left];
   a[left] = tmp;
-  //cout << i << endl;
-  //cout << "left= " << left << endl;
-  //cout << "right= " << right << endl;
   return i-1;
 }
